d3048351a2d86211d26e0a96eb033e628cecb2ed
bonacciog <giovanni.bonaccio96@gmail.com>
2019-11-12 13:57:08 +0100

    Date bug fixed

--- back-end/test/WSClient.js
+++ back-end/test/WSClient.js
@@ -2,26 +2,26 @@ const WebSocket = require('ws');
 
 const ws = new WebSocket('ws://localhost:3000');
 
-/* const message = JSON.stringify({ 
+const message = JSON.stringify({ 
   request : "saveUser", 
   User: {Firstname : 'Giovanni', Lastname : "Bonaccio", University : "Unibo"},
   key : "chiave"
-}); */
-
-const message = JSON.stringify({ 
-  request : "login", 
-  UserID: 1,
-  key : 'chiave'
 });
 
-ws.on('open', function open() {  
- ws.send(message);  
+/* const message = JSON.stringify({
+  request: "login",
+  UserID: 1,
+  key: 'chiave'
+}); */
+
+ws.on('open', function open() {
+  ws.send(message);
 });
 
 ws.on('message', function incoming(data) {
   console.log(data);
 });
 
-ws.on('close', function(){
+ws.on('close', function () {
   console.log("Connection closed")
 })

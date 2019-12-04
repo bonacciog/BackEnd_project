4e3b732d16ef11bd2a0c14d8103725627b2b10cb
bonacciog <giovanni.bonaccio96@gmail.com>
2019-12-04 11:53:37 +0100

    html page added

--- back-end/Server.js
+++ back-end/Server.js
@@ -1,8 +1,9 @@
 const c = require('./controller/Controller');
 const http = require('http');
 const WebSocket = require('ws');
+const { readFileSync } = require('fs')
 
-const ipAddress = 'localhost';
+const ipAddress = '80.211.33.82';
 const port = 3000;
 
 const server = http.createServer().listen(port, ipAddress);
@@ -10,6 +11,14 @@ const wss = new WebSocket.Server({ server });
 
 
 server.on('request', function (req, res) {
+    if (req.method === 'GET') {
+        if (req.url === '/') {
+            let home = readFileSync('./htmlPages/home.html').toString();
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(home);
+        }
+    }
+
     if (req.method == 'POST') {
         var body = '';
     }
@@ -20,12 +29,14 @@ server.on('request', function (req, res) {
 
     req.on('end', function () {
         try {
-            console.log("["+Date(Date.now()).toString()+"] - "+"[Server]: Arrived request:\n" + body)
-	    var req = JSON.parse(body);
-            c.eventRequest.emit(req.request, req, res);
+            if (body !== undefined) {
+                console.log("[" + Date(Date.now()).toString() + "] - " + "[Server]: Arrived request:\n" + body)
+                var req = JSON.parse(body);
+                c.eventRequest.emit(req.request, req, res);
+            }
         }
         catch (err) {
-            console.log("["+Date(Date.now()).toString()+"] - "+"[Server]: Error in trying to serve the request: \n" + err)
+            console.log("[" + Date(Date.now()).toString() + "] - " + "[Server]: Error in trying to serve the request: \n" + err)
         }
     });
 });
@@ -34,14 +45,14 @@ wss.on('connection', function (ws) {
 
     ws.on('message', function (req) {
         try {
-            console.log("["+Date(Date.now()).toString()+"] - "+"[Server]: Arrived request:\n" + req)
+            console.log("[" + Date(Date.now()).toString() + "] - " + "[Server]: Arrived request:\n" + req)
             var request = JSON.parse(req);
             c.eventRequest.emit(request.request, request, this);
         }
         catch (err) {
-            console.log("["+Date(Date.now()).toString()+"] - "+"[Server]: Error in trying to serve the request: \n" + err)
+            console.log("[" + Date(Date.now()).toString() + "] - " + "[Server]: Error in trying to serve the request: \n" + err)
         }
     });
 });
 
-console.log("["+Date(Date.now()).toString()+"] - "+'[Server]: Listening on http://' + ipAddress +':'+port);
+console.log("[" + Date(Date.now()).toString() + "] - " + '[Server]: Listening on http://' + ipAddress + ':' + port);

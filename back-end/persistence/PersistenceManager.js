583e4ecef4a5cfd558f74ba3aeeee2e0883888f9
bonacciog <giovanni.bonaccio96@gmail.com>
2019-12-19 18:27:12 +0100

    bug fixing

--- back-end/persistence/PersistenceManager.js
+++ back-end/persistence/PersistenceManager.js
@@ -1157,7 +1157,7 @@ function getAllChallengesResults(UserID, callback) {
       if (err) callback(err, null);
       console.log("[" + Date(Date.now()).toString() + "] - " + "[PersistenceManager]: Connected to DB!");
     });
-    var sql = "select  C.ID, TopicName, C.ReceiverProposal_ID as Opponent, sum(CR.XP) as MYXP, IF(sum(CR.XP)>=sum(OpponentTable.OpponentXPs),'true','false') as Win\n" +
+    var sql = "select  C.ID, TopicName, C.ReceiverProposal_ID as Opponent, sum(CR.XP) as MYXP, IF(sum(CR.XP)>=OpponentTable.OpponentXPs,'true','false') as Win\n" +
       " from 1001db.Challenge C, 1001db.ChallengeResults CR, 1001db.ChallengeQuestions CQ, 1001db.Topics T, (\n" +
       "select ChallengeID, ReceiverProposal_ID, sum(XP) as OpponentXPs\n" +
       " from 1001db.Challenge C, 1001db.ChallengeResults CR\n" +
@@ -1173,7 +1173,7 @@ function getAllChallengesResults(UserID, callback) {
       "and OpponentTable.ReceiverProposal_ID = C.ReceiverProposal_ID\n" +
       "group by C.ID, PlayerID\n" +
       "UNION\n" +
-      "select  C.ID, TopicName, C.SenderProposal_ID, sum(CR.XP) as MYXP, IF(sum(CR.XP)>=sum(OpponentTable.OpponentXPs),'true','false') as Win\n" +
+      "select  C.ID, TopicName, C.SenderProposal_ID, sum(CR.XP) as MYXP, IF(sum(CR.XP)>=OpponentTable.OpponentXPs,'true','false') as Win\n" +
       "from 1001db.Challenge C, 1001db.ChallengeResults CR, 1001db.ChallengeQuestions CQ, 1001db.Topics T, (\n" +
       "select ChallengeID, SenderProposal_ID, sum(XP) as OpponentXPs\n" +
       "from 1001db.Challenge C, 1001db.ChallengeResults CR\n" +

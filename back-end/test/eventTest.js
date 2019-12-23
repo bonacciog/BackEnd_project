4482a4ae5095fd839427fa8c8210751c5e2ba65e
bonacciog <giovanni.bonaccio96@gmail.com>
2019-12-23 15:13:39 +0100

    starting new project

--- back-end/test/eventTest.js
+++
@@ -1,8 +0,0 @@
-const  EventEmitter = require('events').EventEmitter;
-var eventRequest = new EventEmitter();
-
-eventRequest.on('saveUser', function(req,res){
-    console.log("ciao");
-})
-
-eventRequest.emit("saveUser")
--- back-end/test/userTest.js
+++
@@ -1,5 +0,0 @@
-const userClass = require('../model/User');
-const pm = require('../persistence/PersistenceManager');
-const c = require('../controller/Controller');
-
-
